60d3e2e28b046133b95c6eb971c92e889823f56c
ps9610 <ps961001@gmail.com>
2021-03-15 21:17:53 +0900

    npm 배포 추가

---
+++ node_modules/project_vue_js/03_08_03_11_vue.js/js/app.js
@@ -0,0 +1,17 @@
+Vue.component("todo-footer",{
+    template : `<p>This is another global child component.</p>`
+})
+
+const todo = {
+    template : `<p>This is another local child component.</p>`
+}
+
+const app = new Vue({
+    el : "#app",
+    data : {
+        message : `This is a parent component.`
+    },
+    components : {
+        "todo-list" : todo
+    }
+});
